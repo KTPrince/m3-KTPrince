8c48dafa243ec045db9d85f78268ccccdc817465
KTPrince <ktprince@email.sc.edu>
2021-09-11 12:58:44 -0400

    Basic Expo CLI tutorial, fully functioning.  Needs to be changed to add new features to fulfil milestone.

--- App.js
+++ App.js
@@ -1,11 +1,77 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, Platform, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as Sharing from 'expo-sharing';
+import uploadToAnonymousFilesAsync from 'anonymous-files';
+import logo from'./assets/MesoSphere.png';
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = React.useState(null);
+
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted == false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    
+    if (pickerResult.cancelled == true) {
+      return;
+    }
+
+    if(Platform.OS === 'web') {
+      //alert(`Huh.`);
+      alert(`Unfortunately, web sharing is depreciated.  Check browser console for details (it may take some time to appear).`);
+      let remoteUri = await uploadToAnonymousFilesAsync(pickerResult.uri);
+      console.log(setSelectedImage({ localUri: pickerResult.uri, remoteUri}));
+    } else {
+      setSelectedImage({ localUri: pickerResult.uri, remoteUri: null});
+    }
+  };
+
+
+
+  let openShareDialogAsync = async () => {
+    if (!(await Sharing.isAvailableAsync()) || Platform.OS === 'web') {
+      alert(`This image is available for sharing at: ${selectedImage.remoteUri}`);
+      return;
+    }
+
+    await Sharing.shareAsync(selectedImage.localUri);
+  }
+
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: selectedImage.localUri}}
+          style={styles.thumbnail}
+        />
+        <TouchableOpacity
+          onPress={openShareDialogAsync}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Share this photo</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+        <Image source={logo} style={styles.logo} />
+        <Text style={styles.instructions}>
+          To share a photo from your phone with a friend, just press the button below!
+        </Text>
+        
+        <TouchableOpacity
+          onPress={openImagePickerAsync}
+          style={styles.button}>
+          <Text style={styles.buttonText}>Pick a photo</Text>
+        </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +84,31 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain',
+    marginBottom: 10,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+    textAlign: 'center',
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain",
+  }
 });
